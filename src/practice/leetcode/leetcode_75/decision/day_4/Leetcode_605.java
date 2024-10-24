a901608ce0a71a0593401e13364abe393b4b8e79
Raizer4 <nikitakraska1@gmail.com>
2024-10-24 18:26:09 +0300

    172 days before

---
+++ src/practice/leetcode/leetcode_75/decision/day_4/Leetcode_605.java
@@ -0,0 +1,36 @@
+package practice.leetcode.leetcode_75.decision.day_4;
+
+// Can Place Flowers
+// https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
+
+public class Leetcode_605 {
+
+    public static void main(String[] args) {
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1}, 2));
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1,0,0}, 2));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0){
+                boolean prev = (i == 0 || flowerbed[i - 1] == 0);
+                boolean next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
+
+                if (prev && next){
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+            if (n == 0){
+                return true;
+            }
+
+        }
+
+        return n <= 0;
+    }
+
+}
---
+++ src/practice/leetcode/leetcode_75/decision/day_4/similar/Leetcode_561.java
@@ -0,0 +1,30 @@
+package practice.leetcode.leetcode_75.decision.day_4.similar;
+
+// Array Partition
+// https://leetcode.com/problems/array-partition/description/
+
+import java.util.Arrays;
+
+public class Leetcode_561 {
+
+    public static void main(String[] args) {
+        System.out.println(arrayPairSum(new int[]{
+                6,2,6,5,1,2
+        }));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int k = nums.length / 2;
+        int index = 0;
+        int result = 0;
+
+        while (k != 0){
+            result += nums[index];
+            k--;
+            index += 2;
+        }
+        return result;
+    }
+
+}
