2141293476d1d136660a63acb114ecf5ca7c6f1d
Raizer4 <nikitakraska1@gmail.com>
2024-12-16 19:15:08 +0300

    117 days before

--- src/practice/leetcode/stack/medium/Leetcode_739.java
+++ src/practice/leetcode/stack/medium/Leetcode_739.java
@@ -1,20 +1,35 @@
 package practice.leetcode.stack.medium;
 
+// Daily Temperatures
+// https://leetcode.com/problems/daily-temperatures/description/
+
 import java.util.Stack;
 
 public class Leetcode_739 {
 
     public static void main(String[] args) {
+        int[] ints = dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
 
+        for (int temp : ints){
+            System.out.println(temp);
+        }
     }
 
     public static int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
+       Stack<int[]> stack = new Stack();
 
-        for (int i = 0; i < temperatures.length; i++){
+       int[] res = new int[temperatures.length];
 
-        }
+       for (int i = 0; i < temperatures.length; i++){
+           while (!stack.isEmpty() &&  stack.peek()[0] < temperatures[i]){
+               int[] pop = stack.pop();
+               res[pop[1]] = i - pop[1];
+           }
+           stack.push(new int[]{temperatures[i], i});
+       }
 
+
+       return res;
     }
 
 }
